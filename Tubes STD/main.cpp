74592ebb1f74a5fffb4c0bf0972faa5eea2255ed
yaasinrafi1 <121558931+yaasinrafi1@users.noreply.github.com>
2023-01-02 10:41:30 +0700

    Update main.cpp

--- Tubes STD/main.cpp
+++ Tubes STD/main.cpp
@@ -3,8 +3,32 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(){
+  // Create two lists, one for patients and one for rooms
+  list_P pasienList;
+  list_R ruanganList;
+
+  // Initialize both lists
+  createPasien(pasienList);
+  createRuangan(ruanganList);
+
+  // Create a new patient and add them to the patient list
+  infotype_P p;
+  p.namaPasien = "John Smith";
+  p.idPasien = 1;
+  p.keluhanPasien = "Flu symptoms";
+  adr_pasien newPatient = createElementPasien(p);
+  insertPasien(pasienList, newPatient);
+
+  // Create a new room and add it to the room list
+  infotype_R r;
+  r.kasurTersedia = "Yes";
+  r.noRuang = 101;
+  adrRuangan newRoom = createElementRuangan(r);
+  insertRuangan(ruanganList, p.namaPasien, newRoom);
+
+  // Show all patients and their assigned rooms
+  showAll(ruanganList);
+  showPasien(pasienList);
+  return 0;
 }
